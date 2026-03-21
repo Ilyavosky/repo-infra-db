c0bb4f76d76f58020b0c433fff90b045ae7fe824
Ilyavosky <ilyacortesruiz@gmail.com>
2026-03-21 16:55:44 -0600

    Fix: Arreglar campos de la DB

--- migrations/09_triggers.sql
+++ migrations/09_triggers.sql
@@ -4,14 +4,14 @@ LANGUAGE plpgsql
 AS $$
 DECLARE
   v_precio_adquisicion DECIMAL(12,2);
-  v_precio_etiqueta DECIMAL(12,2);
+  v_precio_etiqueta    DECIMAL(12,2);
 BEGIN
   SELECT precio_adquisicion, precio_venta_etiqueta
   INTO v_precio_adquisicion, v_precio_etiqueta
   FROM variantes
   WHERE id_variante = NEW.id_variante;
 
-  IF NEW.precio_venta_final < v_precio_adquisicion THEN
+  IF NEW.precio_venta_final > 0 AND NEW.precio_venta_final < v_precio_adquisicion THEN
     RAISE EXCEPTION 'Precio de venta (%) no puede ser inferior al precio de adquisicion (%) para variante %',
       NEW.precio_venta_final, v_precio_adquisicion, NEW.id_variante;
   END IF;
